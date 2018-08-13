71c54ba19dc6bf091580a212f3fc1d56a40ef71c
GeovaniiSilva <geapsi@gmail.com>
2018-08-13 20:33:15 -0300

    Make a basic User model with password

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'bootstrap'
 gem 'jquery-rails'
+gem 'bcrypt'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810174324) do
+ActiveRecord::Schema.define(version: 20180812033401) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 20180810174324) do
     t.string "sex"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
